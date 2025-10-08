123d0a3927854da61e689f060787fa3135b4b561
shafiul <shafiul7sms@gmail.com>
2025-10-08 19:53:36 +0600

    Ecommarce Project init

---
+++ app/Helper/ApiResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponse
+{
+    public function success($data = null, $messages = [], $code = 200): JsonResponse
+    {
+        return response()->json([
+            'data' => $data,
+            'messages' => $messages,
+            'code' => $code
+        ]);
+    }
+
+    public function error($messages = [], $code = 422): JsonResponse
+    {
+        return response()->json([
+            'data' => null,
+            'messages' => $messages,
+            'code' => $code
+        ]);
+    }
+}
+
