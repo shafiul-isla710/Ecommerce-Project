71a9868377c46ebe66f4fb3fcea74f679fb2bab9
shafiul <shafiul7sms@gmail.com>
2025-11-18 22:33:50 +0600

    brand and category seeding fixed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp({});
+
+app.component('example-component', ExampleComponent);
+
+app.mount('#app');
